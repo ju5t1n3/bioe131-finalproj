8e9036ac58c9608596c64ab547d735fa7c68c050
touss <toussmajidi@berkeley.edu>
2024-12-08 10:50:42 -0800

    fetch pdb

---
+++ fetch_PDB.py
@@ -0,0 +1,24 @@
+import requests
+import os
+
+def download_pdb_by_id(pdb_id, output_dir="pdb_structures"):
+    """
+    Download the PDB file for a given PDB ID.
+    """
+    os.makedirs(output_dir, exist_ok=True)
+    file_path = os.path.join(output_dir, f"{pdb_id}.pdb")
+    
+    url = f"https://files.rcsb.org/download/{pdb_id}.pdb"
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(file_path, "w") as file:
+            file.write(response.text)
+        print(f"Downloaded PDB file: {pdb_id}")
+    else:
+        print(f"Failed to download PDB file for {pdb_id}: {response.status_code}")
+
+# download_pdb_by_id('5DVW')
+
+
+    
+
